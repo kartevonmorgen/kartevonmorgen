67870e6b57c2fc3189f3dc40e66d6c898b290159
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-02-05 21:54:58 +0100

    refactor(message component): migrate to ES6

---
+++ src/components/Message.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { pure } from "recompose";
+
+class Message extends React.Component {
+
+  render() {
+
+    let actionButton = null;
+    var l;
+
+    if (l = this.props.actionButtonLabel) {
+      actionButton =
+        <button
+          onClick = { (ev) => { ev.preventDefault(); this.props.onAction(); }}
+          className = "pure-button" >
+          <i className = { this.props.actionButtonIcon } />
+          { l }
+        </button>;
+    }
+    return (
+      <div className = 'dialog'>
+        <div className = "message">
+          <i className = { this.props.iconClass } />
+          { this.props.message }
+        </div>
+        <div className = "close-button">
+          <button
+            onClick = { (ev) => { ev.preventDefault(); this.props.onCancel(); }}
+            className = "pure-button" >
+            <i className = "fa fa-ban" />
+            { this.props.cancelButtonLabel }
+          </button>
+          { actionButton }
+        </div>
+      </div>
+    );
+  }
+}
+
+const T = React.PropTypes;
+
+Message.propTypes = {
+  onCancel: T.func,
+  onAction: T.func,
+  actionButtonLabel: T.string,
+  cancelButtonLabel: T.string
+};
+
+module.exports = pure(Message);
