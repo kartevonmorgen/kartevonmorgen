04990834408a0784529bcb101b667360c10684f7
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-02-09 21:06:00 +0100

    refactor(webapi): migrate to ES6

---
+++ src/WebAPI.js
@@ -0,0 +1,138 @@
+const URL = "https://api.ofdb.io/v0";
+const NOMINATIM = "https://nominatim.openstreetmap.org";
+
+import request  from "superagent/lib/client";
+import saPrefix from "superagent-prefix";
+
+const prefix = saPrefix(URL);
+
+const jsonCallback = (cb) => (err, res) => {
+  if (err) {
+    cb(err);
+  } else {
+    cb(null, res.body);
+  }
+};
+
+module.exports = {
+
+  search: (txt, cats, bbox, cb) => {
+
+    if (txt == null)  { txt  = ''; }
+    if (cats == null) { cats = []; }
+    if (bbox == null) { bbox = []; }
+
+    request
+      .get('/search')
+      .use(prefix)
+      .query({ text: txt.trim().split(' ') })
+      .query('categories=' + cats.join(','))
+      .query('bbox=' + bbox.join(','))
+      .set('Accept', 'application/json')
+      .end(jsonCallback(cb));
+  },
+
+  searchAddress: (addr, cb) => {
+
+    if (addr == null) { addr = ''; }
+
+    request
+      .get('/search')
+      .use(saPrefix(NOMINATIM))
+      .query({ q: addr })
+      .query({ format: 'json' })
+      .query({ addressdetails: 1 })
+      .set('Accept', 'application/json')
+      .end(jsonCallback(cb));
+  },
+
+  searchGeolocation: (latlng, cb) => {
+
+    if (latlng == null) {
+      latlng = { lat: 0.0, lng: 0.0 };
+    }
+
+    request
+      .get('/reverse')
+      .use(saPrefix(NOMINATIM))
+      .query({ lat: latlng.lat })
+      .query({ lon: latlng.lng })
+      .query({ zoom: 18 })
+      .query({ format: 'json' })
+      .query({ addressdetails: 1 })
+      .set('Accept', 'application/json')
+      .end(jsonCallback(cb));
+  },
+
+  getEntries: (ids, cb) => {
+
+    if (ids == null) {
+      ids = [];
+    }
+
+    if (!Array.isArray(ids)) {
+      ids = [ids];
+    }
+
+    if (!(ids.length > 0)) {
+      cb(new Error("no IDs were passed"));
+    } else {
+      request
+        .get('/entries/' + ids.join(','))
+        .use(prefix).set('Accept', 'application/json')
+        .end(jsonCallback(cb));
+    }
+  },
+
+  saveNewEntry: (e, cb) => {
+    request
+      .post('/entries/')
+      .use(prefix)
+      .set('Accept', 'application/json')
+      .send(e)
+      .end((err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, res.text);
+        }
+    });
+  },
+
+  saveEntry: (e, cb) => {
+    request
+      .put('/entries/' + e.id)
+      .use(prefix)
+      .set('Accept', 'application/json')
+      .send(e)
+      .end((err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, res.text);
+        }
+    });
+  },
+
+  getAllCategories: (cb) => {
+    request
+      .get('/categories/')
+      .use(prefix)
+      .set('Accept', 'application/json')
+      .end(cb);
+  },
+
+  getServerInfo: (cb) => {
+    request
+      .get('/server/version')
+      .set('Accept', 'application/json')
+      .use(prefix)
+      .end((err, res) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, { version: res.text });
+        }
+    });
+  }
+};
