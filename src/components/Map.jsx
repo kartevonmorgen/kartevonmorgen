9e3b7f44744b4846803eeb29439b7ec4566b54f0
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-04-07 13:39:26 +0200

    refactor(map): migrate to es6

---
+++ src/components/Map.jsx
@@ -0,0 +1,125 @@
+import "./Map.styl"
+
+import React, { Component }         from "react"
+import { Map, TileLayer, Marker }   from "react-leaflet"
+import VMIcons                      from "vm-leaflet-icons"
+import URLs                         from "../constants/URLs"
+import { pure }                     from "recompose"
+import { NAMES, CSS_CLASSES, IDS }  from  "../constants/Categories"
+
+const { INITIATIVE, EVENT, COMPANY } = IDS;
+
+class KVMMap extends Component {
+
+  getIconById(id){
+    switch (id) {
+      case INITIATIVE:
+        return VMIcons.initiative;
+      case EVENT:
+        return VMIcons.event;
+      case COMPANY:
+        return VMIcons.company;
+      default:
+        return VMIcons.unknown;
+    }
+  }
+
+  componentDidMount(){
+    //workaround due to a bug in react-leaflet:
+    const map = this.refs.map;
+    if (map) {
+      map.fireLeafletEvent('load', map)
+      map.getLeafletElement().addControl(L.control.zoom({position: 'bottomright'}))
+      this.props.onMoveend(this.getMapCoordinates())
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevProps.size != this.props.size) {
+      this.refs.map.getLeafletElement().invalidateSize()
+    }
+  }
+
+  getMapCoordinates(){
+    const m = this.refs.map.getLeafletElement()
+    return {
+      center: m.getCenter(),
+      bbox  : m.getBounds(),
+      zoom  : m.getZoom()
+    }
+  }
+
+  render() {
+
+    var markers = [];
+
+    const {
+      entries,
+      center,
+      zoom,
+      marker,
+      highlight,
+      onMoveend,
+      onZoomend,
+      onClick,
+      onMarkerClick
+    } = this.props;
+
+    if (entries && entries.length > 0 ) {
+      entries.forEach(e => {
+        markers.push(
+          <Marker
+            onClick   = { () => { onMarkerClick(e.id) }}
+            key       = { e.id }
+            position  = {{ lat: e.lat, lng: e.lng }}
+            opacity   = { highlight.length > 0
+                            ? highlight.indexOf(e.id) < 0 ? 0.3 : 1
+                            : 1
+                        }
+            icon      = { this.getIconById(e.categories[0]) }
+            />
+          )
+      })
+    }
+
+    return (<Map
+      style       = {{ height: "100%" }}
+      ref         = 'map'
+      center      = { center }
+      zoom        = { zoom   }
+      zoomControl = { false }
+      className   = "map"
+      onMoveend   = { (e) => { onMoveend(this.getMapCoordinates()) }}
+      onZoomend   = { (e) => { onZoomend(this.getMapCoordinates()) }}
+      onClick     = { (e) => { onClick(e.latlng) }} >
+
+        <TileLayer
+          url = { URLs.OSM_TILES.link }
+          attribution = {
+            '&copy; <a class="osm attr" href=' +
+             URLs.OSM_ATTR.link + '>' + URLs.OSM_ATTR.name + '</a>' } >
+        </TileLayer>
+        { markers }
+        { marker
+          ? <Marker position = { marker } icon = { this.getIconById(parseInt(this.props.category)) } />
+          : null
+        }
+      </Map>)
+    }
+}
+
+const T = React.PropTypes;
+
+KVMMap.propTypes = {
+    entries       : T.array,
+    highlight     : T.array,
+    center        : T.object,
+    zoom          : T.number,
+    marker        : T.object,
+    onClick       : T.func,
+    onMoveend     : T.func,
+    onZoomend     : T.func,
+    onMarkerClick : T.func
+};
+
+module.exports = pure(KVMMap);
