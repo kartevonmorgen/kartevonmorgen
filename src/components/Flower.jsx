db55935881f904fe656acd17c4e73c791a613239
Markus Kohlhase <markus.kohlhase@slowtec.de>
2017-05-16 13:25:57 +0200

    fix(*): add missing Flower component

---
+++ src/components/Flower.jsx
@@ -0,0 +1,90 @@
+import Flower from "react-vm-flower";
+import React  from "react";
+
+const rating_groups = (ratings=[]) => {
+  var groups = {};
+  ratings
+    .filter(r => typeof r !== "undefined" && r !== null)
+    .forEach(r =>{
+      if (groups[r.context] == null) {
+        groups[r.context] = [];
+      }
+      groups[r.context].push(r);
+    });
+  return groups;
+}
+
+const flower_scales = (ratings=[]) => {
+  var scales = [0.6,0.6,0.6,0.6,0.6,0.6];
+  const groups = rating_groups(ratings);
+  Object.keys(groups).forEach(g => {
+    const rtngs = groups[g];
+    if (rtngs && rtngs.length > 0) { 
+      const av = rtngs.reduce((acc, r) => {return acc+r.value;}, 0) / rtngs.length;
+      const scale = 0.233 * av + 0.533;
+      switch (g) {
+        case "diversity":
+          scales[0] = scale;
+          break;
+        case "renewable":
+          scales[1] = scale;
+          break;
+        case "fairness":
+          scales[2] = scale;
+          break;
+        case "humanity":
+          scales[3] = scale;
+          break;
+        case "transparency":
+          scales[4] = scale;
+          break;
+        case "solidarity":
+          scales[5] = scale;
+          break;
+        default:
+          break;
+      }
+    }
+  })
+  return scales;
+}
+
+const KVMFlower = (ratings=[],radius=40) => {
+
+  var colors = ["#eee", "#eee", "#eee", "#eee", "#eee", "#eee"];
+
+  ratings.filter(r => typeof r !== "undefined" && r !== null).forEach(r => {
+    switch (r.context) {
+      case "diversity":
+        colors[0] = null;
+        break;
+      case "renewable":
+        colors[1] = null;
+        break;
+      case "fairness":
+        colors[2] = null;
+        break;
+      case "humanity":
+        colors[3] = null;
+        break;
+      case "transparency":
+        colors[4] = null;
+        break;
+      case "solidarity":
+        colors[5] = null;
+        break;
+      default:
+        break;
+    }
+  })
+
+  return (
+    <svg width={(radius +2) * 2} height = {(radius+2) * 2}>
+      <g transform={"translate(" + (radius + 2) + "," + (radius + 2) + ")"}>
+        <circle cx={0} cy={0} r={radius+1} fill="#fff" stroke="#ccc" strokeWidth={0.5} />
+        <Flower size = {radius * 2} scales={flower_scales(ratings)} colors={colors} />
+      </g>
+    </svg>)
+}
+
+module.exports = KVMFlower
