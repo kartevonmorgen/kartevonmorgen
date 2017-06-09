730400ae1d446a2f26c339265c04b042b5724122
David Ziegler <david-ziegler@posteo.de>
2017-06-09 17:12:55 +0200

    new(routing): route by tags

--- src/Actions.js
+++ src/Actions.js
@@ -403,9 +403,10 @@ const Actions = {
       let url_parsed = parseURL(newURL);
       let { params } = url_parsed;
       const { entry } = params;
+      const { tags } = params;
       const mapCenter = params["map-center"];
-      const entries = getState().server.entries;
 
+      const entries = getState().server.entries;
       if(entry){ 
         if(entries[entry] == null){
           WebAPI.getEntries([entry], (err, res) => {
@@ -427,14 +428,26 @@ const Actions = {
           dispatch(Actions.setCurrentEntry(entry));
         }
       }
-      
-      if (!entry && mapCenter && mapCenter.length > 2) {
-        let [lat, lng] = mapCenter.split(',');
-        lat = parseFloat(lat);
-        lng = parseFloat(lng);
-        if (!(isNaN(lat) || isNaN(lng))) {
-          dispatch(Actions.setCenter({lat, lng}));
-          dispatch(Actions.setBbox(getState().map.bbox));
+      else {
+        if (mapCenter && mapCenter.length > 2) {
+          let [lat, lng] = mapCenter.split(',');
+          lat = parseFloat(lat);
+          lng = parseFloat(lng);
+          if (!(isNaN(lat) || isNaN(lng))) {
+            dispatch(Actions.setCenter({lat, lng}));
+            dispatch(Actions.setBbox(getState().map.bbox));
+            dispatch(Actions.search());
+          }
+        }
+
+        if (tags && tags.length > 0) {
+          let tags2 = tags.split(',');
+          let txt = "";
+          for (let tag of tags2) {
+            txt += ("#" + tag);
+          }
+          dispatch(Actions.setCurrentEntry());
+          dispatch(Actions.setSearchText(txt));
           dispatch(Actions.search());
         }
       }
