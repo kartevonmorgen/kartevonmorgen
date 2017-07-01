6642dae28baec88d28b8206de259a0eaa6d96d15
David Ziegler <david-ziegler@posteo.de>
2017-07-01 11:27:33 +0200

    fix(routing): first render page, then do initial routing

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ if(window.location.hash == ""){
   store.dispatch(Actions.showMenu());
 }
 
+ReactDOM.render(Wrapper, rootElement);
+
 // read the hash url and navigate to the right page
 store.dispatch(Actions.updateStateFromURL);
 Router.route();
-
-ReactDOM.render(Wrapper, rootElement);
