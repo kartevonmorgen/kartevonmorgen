29127b9b80792a6c02e91512fda4fab1eadca472
David Ziegler <david-ziegler@posteo.de>
2017-07-05 10:24:05 +0200

    new(login): add logout (2)

---
+++ src/util/cookies.js
@@ -0,0 +1,11 @@
+const deleteCookie = ( name ) => {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+const deleteAllCookies = () => {
+  document.cookie.split(";").forEach( c => {
+    deleteCookie(c.split("=")[0]);
+  });
+}
+
+module.exports = { deleteCookie, deleteAllCookies }
