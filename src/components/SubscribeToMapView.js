ccb6eaf7715e3fc1aac9063d2f8c23b34779a6c6
David Ziegler <david-ziegler@posteo.de>
2017-07-06 11:50:28 +0200

    new(login): add element in the left pane for subscription to map view

---
+++ src/components/SubscribeToMapView.js
@@ -0,0 +1,16 @@
+import React    from "react"
+import Actions  from "../Actions"
+
+const SubscribeToMapView = () => {
+  return ( 
+    <div className= "subscribe-to-map-view">
+      <h3>Kartenausschnitt abonnieren</h3>
+      <p>Navigiere zu dem Kartenausschnitt der dich interessiert und 
+      klicke dann auf "Kartenausschnitt abonnieren".</p>
+      <p>Du wirst dann per Email über neue Einträge und Änderungen in diesem
+      Kartenbereich informiert. Dieses Abonnement kannst du jederzeit wieder 
+      abbestellen.</p>
+    </div>)
+}
+
+module.exports = SubscribeToMapView
