kartevonmorgen/kartevonmorgen

commit e96e902609c759be2aca1a037151b9de1c618302

new(Searchbar): show searchbar only when in search-result mode

src/components/App.coffee
@@ -132,8 +132,8 @@ Main = React.createClass
 
         div className:"left #{if view.showLeftPanel and not view.menu then 'opened' else 'closed'}",
 
-          div className: "search #{
-            if view.left? then 'integrated' else 'standalone'
+          div className: "search integrated #{
+            if view.left in [V.RESULT] then 'open' else 'closed'
           }",
             React.createElement SearchBar,
               searchText      : search.text

src/components/App.styl
@@ -347,8 +347,7 @@ button.pure-button i
         position        relative
         z-index         1
         overflow-y      scroll
-        height          calc(100vh - 6.65em)
-        padding-bottom  6.65em;
+        height          100%
         box-sizing  border-box
         background      #fff
 
@@ -361,16 +360,12 @@ button.pure-button i
         .menu
           width       100%
 
-      .search.standalone
-        left     1em
-        top      1em
-        width    350px
-        z-index  2
-        position absolute
-
       .search
         position  relative
         z-index   2
+
+        &.closed
+          display none
 
         .main-categories
           height 2.1em

commit e54a717a32b41a4d824fcdf41e268fa6765ac9cd

fix(styling): fix height of left sidebar (bug was introduced by e96e902)

src/components/App.coffee
@@ -277,7 +277,7 @@ Main = React.createClass
                           "abonnieren"
                     ]
 
-          div className:"content",
+          div className: "content-wrapper",
 
             switch view.left
 
@@ -320,7 +320,7 @@ Main = React.createClass
                         onMouseEnter: (id) -> dispatch Actions.highlight id
                         onMouseLeave: (id) -> dispatch Actions.highlight()
               when V.ENTRY
-                div null,
+                div className: "content",
                   React.createElement EntryDetails,
                     entry   : entries[search.current] || null
                   React.createElement Ratings,
@@ -329,60 +329,64 @@ Main = React.createClass
                     onRate  : (id) => dispatch Actions.showNewRating id
 
               when V.EDIT, V.NEW
-                React.createElement EntryForm,
-                  ref: 'form'
-                  isEdit: form[EDIT.id]?.kvm_flag_id?
-                  onSubmit: (data) ->
-                    dispatch Actions.saveEntry
-                      id          : form[EDIT.id]?.kvm_flag_id
-                      title       : data.title
-                      description : data.description
-                      tags        : data.tags?.split(',')
-                      homepage    : data.homepage
-                      telephone   : data.telephone
-                      lat         : Number data.lat
-                      lng         : Number data.lng
-                      street      : data.street
-                      city        : data.city
-                      email       : data.email
-                      zip         : data.zip
-                      version     : (form[EDIT.id]?.values?.version or 0) + 1
-                      categories  : [data.category]
+                div className: "content",
+                  React.createElement EntryForm,
+                    ref: 'form'
+                    isEdit: form[EDIT.id]?.kvm_flag_id?
+                    onSubmit: (data) ->
+                      dispatch Actions.saveEntry
+                        id          : form[EDIT.id]?.kvm_flag_id
+                        title       : data.title
+                        description : data.description
+                        tags        : data.tags?.split(',')
+                        homepage    : data.homepage
+                        telephone   : data.telephone
+                        lat         : Number data.lat
+                        lng         : Number data.lng
+                        street      : data.street
+                        city        : data.city
+                        email       : data.email
+                        zip         : data.zip
+                        version     : (form[EDIT.id]?.values?.version or 0) + 1
+                        categories  : [data.category]
               when V.NEW_RATING
-                React.createElement RatingForm,
-                  ref         : 'rating'
-                  entryid     : form[RATING.id]?.kvm_flag_id
-                  entryTitle  : entries[form[RATING.id]?.kvm_flag_id]?.title
-                  onSubmit: (data) ->
-                    dispatch Actions.createRating
-                      entry   : form[RATING.id]?.kvm_flag_id
-                      title   : data.title
-                      context : data.context
-                      value   : data.value
-                      comment : data.comment
-                      source : data.source
-                  contextToExplain: explainRatingContext
-                  selectedContext: selectedContext
-                  changeContext: (ratingContext) ->
-                    dispatch Actions.explainRatingContext(ratingContext)
+                div className: "content",
+                  React.createElement RatingForm,
+                    ref         : 'rating'
+                    entryid     : form[RATING.id]?.kvm_flag_id
+                    entryTitle  : entries[form[RATING.id]?.kvm_flag_id]?.title
+                    onSubmit: (data) ->
+                      dispatch Actions.createRating
+                        entry   : form[RATING.id]?.kvm_flag_id
+                        title   : data.title
+                        context : data.context
+                        value   : data.value
+                        comment : data.comment
+                        source : data.source
+                    contextToExplain: explainRatingContext
+                    selectedContext: selectedContext
+                    changeContext: (ratingContext) ->
+                      dispatch Actions.explainRatingContext(ratingContext)
               when V.WAIT
-                React.createElement Message,
-                  iconClass: "fa fa-spinner fa-pulse"
-                  message: " lade Daten vom Server ..."
-                  buttonLabel: "abbrechen"
-                  onCancel: ->
-                    dispatch Actions.cancelWait()
+                div className: "content",
+                  React.createElement Message,
+                    iconClass: "fa fa-spinner fa-pulse"
+                    message: " lade Daten vom Server ..."
+                    buttonLabel: "abbrechen"
+                    onCancel: ->
+                      dispatch Actions.cancelWait()
               when V.IO_ERROR
-                React.createElement Message,
-                  iconClass: "fa fa-exclamation-triangle"
-                  message: " Server nicht erreichbar. " +
-                    "Bitte prüfe Deine Internetverbindung " +
-                    "oder versuche es später nochmal. "
-                  buttonLabel: "schließen"
-                  onCancel: ->
-                    dispatch Actions.closeIoErrorMessage()
+                div className: "content",
+                  React.createElement Message,
+                    iconClass: "fa fa-exclamation-triangle"
+                    message: " Server nicht erreichbar. " +
+                      "Bitte prüfe Deine Internetverbindung " +
+                      "oder versuche es später nochmal. "
+                    buttonLabel: "schließen"
+                    onCancel: ->
+                      dispatch Actions.closeIoErrorMessage()
               when V.SUBSCRIBE_TO_BBOX
-                div className: "subscribe-to-bbox",
+                div className: "content subscribe-to-bbox",
                   React.createElement SubscribeToBbox,
                     subscriptionExists: user.subscriptionExists
         div className:"hide-sidebar",

src/components/App.styl
@@ -343,13 +343,28 @@ button.pure-button i
       overflow-y  hidden
       float left
 
-      .content
-        position        relative
-        z-index         1
-        overflow-y      scroll
-        height          100%
-        box-sizing  border-box
-        background      #fff
+      .content-wrapper
+        height        100vh
+        background    #fff
+        position      relative
+        z-index       2
+
+        .result
+          position        relative
+          z-index         2
+          overflow-y      scroll
+          height          calc(100vh - 107px)
+          box-sizing      border-box
+          padding-bottom  30px
+
+        .content
+          position        relative
+          z-index         2
+          overflow-y      scroll
+          height          calc(100vh - 42px)
+          box-sizing      border-box
+          background      #fff
+          padding-bottom  30px
 
       &.closed
         width 0