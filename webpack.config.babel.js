869160afcc6f3b8c45603eb32d928dc8e8095029
David Ziegler <david-ziegler@posteo.de>
2018-02-08 13:00:46 +0100

    fix(ie11): use object.assign polyfill

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -44,13 +44,14 @@ const config = {
         test:   /\.jsx?$/,
         loader: "babel-loader",
         query: {
-          plugins: ['transform-decorators-legacy']
+          plugins: ['transform-decorators-legacy', 'transform-object-assign']
         },
         include: [
           path.resolve(__dirname, "src"),
           path.resolve(__dirname, "spec"),
           path.resolve(__dirname, "node_modules/flash-notification-react-redux"),
-          path.resolve(__dirname, "node_modules/vm-leaflet-icons")
+          path.resolve(__dirname, "node_modules/vm-leaflet-icons"),
+          path.resolve(__dirname, "node_modules/redux-logger")
         ],
       },
       {
