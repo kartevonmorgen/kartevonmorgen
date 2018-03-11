d0e741753280d7a48c64b60ebb5e0a53a677cae3
David Ziegler <david-ziegler@posteo.de>
2018-03-11 17:34:11 +0100

    fix(IE-bug): IE<=10 is not able to access window.location.origin

--- src/WebAPI.js
+++ src/WebAPI.js
@@ -1,3 +1,7 @@
+// for Internet Explorer:
+if (!window.location.origin) {
+  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+}
 const URL = location.origin + "/api";
 
 const NOMINATIM_URL = "https://nominatim.openstreetmap.org";
