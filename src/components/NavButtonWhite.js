fd193723517142c3b74e865a46575230141319e9
David Ziegler <david-ziegler@posteo.de>
2018-08-03 15:37:42 +0200

    fix(styling): show buttons above image and without image correctly

--- src/components/NavButtonWhite.js
+++ src/components/NavButtonWhite.js
@@ -12,11 +12,11 @@ const NavigationButtonWrapperRight = styled.div`
 `;
 
 const NavigationButton = styled.span`
-  color: #fff;
+  color: ${props => props.aboveImage ? "#fff" : "#888"};
   cursor: pointer;
   padding: 4px 7px;
   border-radius: 2px;
-  text-shadow: 1px 1px 5px rgba(0,0,0,0.5);
+  text-shadow: ${props => props.aboveImage ? "1px 1px 5px rgba(0,0,0,0.5)" : "none"};
 
   :hover {
     color: #000;
@@ -26,12 +26,13 @@ const NavigationButton = styled.span`
   }
 `;
 
-const NavButton = ({ keyName, buttonRight, icon, text, onClick }) => {
+const NavButton = ({ keyName, buttonRight, icon, text, onClick, aboveImage }) => {
   const button = buttonRight ? 
     <NavigationButtonWrapperRight>
       <NavigationButton
         key = { keyName }
-        onClick = { onClick }>
+        onClick = { onClick }
+        aboveImage = { aboveImage }>
         <i className= { icon }></i>
         { " " + text }
       </NavigationButton>
@@ -40,7 +41,8 @@ const NavButton = ({ keyName, buttonRight, icon, text, onClick }) => {
     <NavigationButtonWrapper>
       <NavigationButton
         key = { keyName }
-        onClick = { onClick }>
+        onClick = { onClick }
+        aboveImage = { aboveImage }>
         <i className= { icon }></i>
         { " " + text }
       </NavigationButton>
