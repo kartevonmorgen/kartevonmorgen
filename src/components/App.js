kartevonmorgen/kartevonmorgen

commit 3c84f3d15b13bd6a87683d4df8d664576767dad7

fix(styling/Sidebar): make it possible to resize a whole iFrame-content to a calculated value like "height: calc(100vh - 100px)"

src/components/App.js
@@ -323,7 +323,6 @@ const LeftPanel = styled.div `
   position: relative;
   z-index: 2;
   order: -1;
-  height: 100vh;
   overflow-y: hidden;
   float: left;
   background-color: #fff;

commit a946072dcd57f63f7774beb027ca528e7ef4b3fc

fix(Sidebar): fix CSS such that EntryDetails-page is shown again (bug introduced by 3c84f3d)

src/components/App.js
@@ -372,7 +372,6 @@ const LeftPanel = styled.div `
       width: 100%;
       box-sizing: border-box;
       padding-bottom: 1rem;
-      position: absolute;
       display: flex;
       flex-direction: column;
       ::-webkit-scrollbar {