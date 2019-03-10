9c708849ec78e5eea94c68772f69c7dbee185093
Mario Rothauer <marothauer@gmail.com>
2019-03-10 15:48:37 +0100

    fixes #541 - small village scharnstein was not shown in search result

--- src/constants/Search.js
+++ src/constants/Search.js
@@ -1,6 +1,6 @@
 module.exports = {
   CITY_SEARCH_RESULTS_MIN_DISTANCE: 0.3,
-  CITY_SEARCH_RESULTS_MIN_IMPORTANCE: 0.5,
+  CITY_SEARCH_RESULTS_MIN_IMPORTANCE: 0.48,
   NUM_ENTRIES_TO_FETCH: 35,
   NUM_ENTRIES_TO_SHOW: 35 // after a few searches we will have loaded n * NUM_ENTRIES_TO_FETCH, that's why we need this extra limit
 };
