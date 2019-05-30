e723f0cea73d92242bfe6c7893d9b485366e4614
Benedikt Dertinger <post@benediktdertinger.com>
2019-05-30 16:34:47 +0200

    core / test / add http and core tests and data

---
+++ goodmap-core/__tests__/data/sampleBboxIds.ts
@@ -0,0 +1,5 @@
+const smapleBboxIds = [
+  'cd1ac0d81679479fb85acdf59ce69a01',
+];
+
+export default smapleBboxIds;
---
+++ goodmap-core/__tests__/data/sampleEntry.ts
@@ -0,0 +1,26 @@
+import IEntry from '../../src/interfaces/IEntry';
+
+const sampleEntry: IEntry = {
+  categories: ['77b3c33a92554bcf8e8c2c86cedd6f6f'],
+  city: 'München',
+  country: null,
+  created: 1558726214,
+  description: 'Erntegemeinschaft im Sinne einer Solidarische Landwirtschaft durch Bio-Gemüsegärtnerei, die eigene, regionale Lebensmitteln produziert. Daneben eine Bio-Bäckerei und Akademie.',
+  email: 'info@kartoffelkombinat.de',
+  homepage: 'http://www.kartoffelkombinat.de/',
+  id: 'cd1ac0d81679479fb85acdf59ce69a01',
+  image_link_url: null,
+  image_url: 'https://www.kartoffelkombinat.de/blog/wp-content/uploads/2012/04/120415_KK_header_1000x288.jpg',
+  lat: 48.178974179634345,
+  license: 'CC0-1.0',
+  lng: 11.48422332084003,
+  ratings: ['d2523e2e35ad4dd3a3bbd730ef72ff33', 'e4ed7e82ffb4407eb92b1680098e9940'],
+  street: 'Von-Kahr-Straße 24a',
+  tags: ['gemüse', 'genossenschaft', 'gwö', 'gwö-bayern', 'regional', 'saisonal', 'solawi'],
+  telephone: '+49 89 215 46 449',
+  title: 'Kartoffelkombinat eG',
+  version: 7,
+  zip: '80997',
+};
+
+export default sampleEntry;
