41adf9dc00765e92dc5b98af04ed6c2a060d8a9e
Ingo Wonner <iw@nualabs.io>
2019-06-07 17:03:59 +0200

    webapp / new / add basic mst logic

--- goodmap-webapp/src/index.tsx
+++ goodmap-webapp/src/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import makeInspectable from 'mobx-devtools-mst';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import AppStore from './stores/AppStore';
+import ViewStore from './stores/ViewStore';
+import EntryStore from './stores/EntryStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create AppStore with default snapshot
+const store = AppStore.create({
+  ViewStore: ViewStore.create({
+    page: 'home',
+  }),
+  EntryStore: EntryStore.create(),
+});
+
+// connect mobx-devtools-mst
+if (process.env.NODE_ENV === 'development') {
+  console.log('dev')
+  makeInspectable(store);
+}
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
