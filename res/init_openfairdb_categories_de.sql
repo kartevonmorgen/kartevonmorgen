2049b7b2c3de9d64bedb072992e56cd725066b61
Uwe Klotz <uwe.klotz@slowtec.de>
2019-07-01 18:48:31 +0200

    Add SQL commands for initial database setup

---
+++ res/init_openfairdb_categories_de.sql
@@ -0,0 +1,3 @@
+INSERT OR IGNORE INTO categories (id, created, version, name) VALUES ('2cd00bebec0c48ba9db761da48678134', strftime('%s', CURRENT_TIMESTAMP), 0, 'Initiative');
+INSERT OR IGNORE INTO categories (id, created, version, name) VALUES ('c2dc278a2d6a4b9b8a50cb606fc017ed', strftime('%s', CURRENT_TIMESTAMP), 0, 'Event');
+INSERT OR IGNORE INTO categories (id, created, version, name) VALUES ('77b3c33a92554bcf8e8c2c86cedd6f6f', strftime('%s', CURRENT_TIMESTAMP), 0, 'Unternehmen');
