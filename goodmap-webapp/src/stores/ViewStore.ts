0e23d968805b243aec526d1b298dfe1857b8f43b
Ingo Wonner <iw@nualabs.io>
2019-07-09 18:15:33 +0200

    webapp / new / update ViewStore

--- goodmap-webapp/src/stores/ViewStore.ts
+++ goodmap-webapp/src/stores/ViewStore.ts
@@ -1,7 +1,27 @@
 import { types } from 'mobx-state-tree';
+import MapPositionModel from '../models/MapPositionModel';
+import AreaModel from '../models/AreaModel';
 
-const ViewStore = types.model('ViewStore', {
-  page: types.string,
-});
+const ViewStore = types
+  .model('ViewStore', {
+    selectedArea: types.maybe(types.reference(AreaModel)),
+    mapPosition: types.maybe(MapPositionModel),
+  })
+  .actions(self => {
+    /* eslint-disable no-param-reassign */
+
+    // TODO: better solution for any?
+    const setArea = (areaId: any) => {
+      self.selectedArea = areaId;
+      self.mapPosition = {
+        lat: 0,
+        lng: 0,
+      };
+    };
+
+    return {
+      setArea,
+    };
+  });
 
 export default ViewStore;
