46622dec278abd51e648169002795f36f0bfc4fe
Miguel San Miguel <development@miguelsanmiguel.com>
2019-12-12 15:53:29 +0100

    Add button newComment
    FSome files were
    automatically reformatted by the editor

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -160,6 +160,7 @@ class Sidebar extends Component {
                     return ratings[id];
                   })}
                   onRate={ id => { return dispatch(Actions.showNewRating(id)); }}
+                  onComment={ context => { return dispatch(Actions.showNewComment(context)); }}
                 />
               : ''}
               { !isEvent ?
