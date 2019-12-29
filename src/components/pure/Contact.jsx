3a311850ba97e8079b757465d646a34239ffd2da
Helmut Wolman <helmut@bildungsagenten.com>
2019-12-29 22:18:10 +0100

    New Contactdaten
    The old adress I removed and linked to the webpage of Ideen³ instead

--- src/components/pure/Contact.jsx
+++ src/components/pure/Contact.jsx
@@ -14,18 +14,18 @@ const Contact = () =>
     <p>
       {t("text1")}<br /><br />
       Helmut Wolman  <br />
-      Turley-Platz 9  <br />
-      68167 Mannheim  <br />
+      <a href = 'http://ideenhochdrei.org/de/kontakt/'>Ideen³ e.V. // Räume für Entwicklung</a>  <br />
+      Tel: +49 1573-4448245  <br />
 
       <a href = 'https://www.kartevonmorgen.org/#/?entry=0b89e6d0945740b3a77f240d9cdab0e9&zoom=15.00'>{t("showOnKVM")}</a>
     </p>
     <p>
 
-      <FontAwesomeIcon icon={['fab', 'facebook']} />
-      {" "}<a target="_blank" href = { URLs.FACEBOOK.link }>{ URLs.FACEBOOK.name }</a>
-      <br />
-      <FontAwesomeIcon icon="envelope" />
+     <FontAwesomeIcon icon="envelope" />
       {" "}<a target="_blank" href = {URLs.MAIL.link}>{ URLs.MAIL.name }</a>
+      <br />
+       <FontAwesomeIcon icon={['fab', 'facebook']} />
+      {" "}<a target="_blank" href = { URLs.FACEBOOK.link }>{ URLs.FACEBOOK.name }</a>
       <br />
       <FontAwesomeIcon icon={['fab', 'github']} />
       {" "}<a target="_blank" href = {URLs.REPOSITORY.link}>{ URLs.REPOSITORY.name }</a>
