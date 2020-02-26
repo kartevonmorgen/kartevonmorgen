3151995f3a5f11e5e116bc3d4b8fc21552adcd0a
Regenduft <fjgh@regenduft.de>
2020-02-26 23:20:54 +0100

    Revert "fix: apply redux-devtools-extension."
    This reverts commit b8972acc28c1bc2192c8e52f8f54941696471257.

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,6 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from "./reducers";
 import thunk    from "redux-thunk"; // lets us dispatch() functions
 import logger   from "redux-logger";
-
-import reducers from "./reducers";
-
 
 let middlewares = [thunk];
 
@@ -15,13 +12,10 @@ if (__STAGE__ === APP_STAGES.LOCAL) {
 }
 
 // https://github.com/zalmoxisus/redux-devtools-extension
-const composeEnhancers = composeWithDevTools({
-  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-});
-const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
-  applyMiddleware(...middlewares),
-  // other store enhancers if any
-));
+const createStoreWrapper =
+  window.devToolsExtension ? window.devToolsExtension(createStore) : createStore;
+
+const store = applyMiddleware.apply(null,middlewares)(createStoreWrapper)(reducers);
 
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
