2afd0d07217e527454bace0571ca58ffb38c0d68
Navid Kalaei <navidkalaei@gmail.com>
2020-03-26 11:53:00 +0430

    feat: add generate open hours button to EntryForm.jsx

--- src/components/App.js
+++ src/components/App.js
@@ -453,6 +453,13 @@ const StyledApp = styled.div `
     }
   }
 
+  .button-secondary {
+    color: white;
+    border-radius: 4px;
+    text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+    background: rgb(66, 184, 221); /* this is a light blue */
+  }
+
   .fa {
     font-family: "FontAwesome" !important;
   }
