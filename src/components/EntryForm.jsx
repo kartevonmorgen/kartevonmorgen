d23961e87cd2c9d7bef0c66a84e8e1a1ea13863b
Navid Kalaei <navidkalaei@gmail.com>
2020-03-28 19:52:55 +0430

    translation: add opening hours translation to the entry form

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -373,7 +373,7 @@ class Form extends Component {
                     <FontAwesomeIcon icon="clock" />
                   </OptionalFieldLabel>
                   <div className= "pure-u-22-24">
-                    <FieldElement name="opening_hours" className="pure-input-1 optional" component="input" placeholder="Opening Hours" />
+                    <FieldElement name="opening_hours" className="pure-input-1 optional" component="input" placeholder={t("openingHours")} />
                     <FieldElement name="opening_hours" component={errorMessage} />
                     <a
                       className={classNames("pure-u-1-1", "pure-button", "button-secondary")}
