2c5c2576d5dec2cba9dca82adddfe6fda8b97f12
Navid Kalaei <navidkalaei@gmail.com>
2020-07-25 17:50:34 +0430

    feat: keep search filters on entering a new enty

--- src/util/constructUrl.js
+++ src/util/constructUrl.js
@@ -20,7 +20,8 @@ export default (entry, center, zoom, searchText, showLeft, categories) => {
   let params = [];
   if (entry && entry != "NONE") {
     params.push("entry=" + entry);
-  } else if (center) {
+  }
+  if (center) {
     params.push("center=" + center.lat.toFixed(mapConst.NUM_DECIMALS_FOR_COORDINATES)
       + "," +  center.lng.toFixed(mapConst.NUM_DECIMALS_FOR_COORDINATES));
   }
@@ -31,9 +32,10 @@ export default (entry, center, zoom, searchText, showLeft, categories) => {
     params.push("categories=" + categories);
   }
   // if ((entry && entry != "NONE") {
-  if (!(entry && entry != "NONE") && searchText) {
+  if (searchText) {
     params.push(searchTextToUrlQuery(searchText));
   }
+
   if (showLeft != null) {
     params.push("left=" + (showLeft ? "show" : "hide"));
   }
