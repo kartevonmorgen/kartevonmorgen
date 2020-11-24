9ed32bf3c91a3d7344a3b05241f784a93782294e
Navid Kalaei <navidkalaei@gmail.com>
2020-11-24 12:25:02 +0330

    chore kartevonmorgen/kartevonmorgen#776: make the webpack analyzer optional on pack-beta

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,7 @@ import path        from "path"
 import webpack     from "webpack"
 import HTMLPlugin  from 'html-webpack-plugin'
 import CopyPlugin  from 'copy-webpack-plugin'
-import  {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
+// import  {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 import { APP_STAGES } from "./src/constants/App"
 
 let plugins = [];
@@ -104,7 +104,6 @@ const config = {
       leaflet$: path.resolve(__dirname, "node_modules/leaflet/dist/leaflet.js")
     }
   },
-  devtool: "source-map"
 };
 
 let htmlPluginOptions = {
@@ -154,7 +153,7 @@ switch (process.env.NODE_ENV) {
 
   case APP_STAGES.BETA:
     serverStage('beta', APP_STAGES.BETA);
-    plugins.push(new BundleAnalyzerPlugin())
+    // plugins.push(new BundleAnalyzerPlugin())
     break;
 
   default: // production
