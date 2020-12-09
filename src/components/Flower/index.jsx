41689d18c0cc3cba1aa4c7df652a8d231a09c497
Navid Kalaei <navidkalaei@gmail.com>
2020-12-09 09:37:17 +0330

    fix kartevonmorgen/kartevonmorgen#776: do not keep the category selector always open

--- src/components/Flower/index.jsx
+++ src/components/Flower/index.jsx
@@ -1,76 +1,81 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
 import Leaf from "./FlowerLeaf"
-import { rating_groups } from "../../rating_groups"
+import {rating_groups} from "../../rating_groups"
 import Tooltip from "react-tooltip"
 import T from "prop-types"
-import styled     from "styled-components"
-import i18n                 from "../../i18n";
-import STYLE        from "../styling/Variables"
-const colors = STYLE.rating_contexts;
+import styled from "styled-components"
+import i18n from "../../i18n"
+import STYLE from "../styling/Variables"
+
+const colors = STYLE.rating_contexts
 
 //styles below
 
 class Flower extends Component {
-  
+
   static defaultProps = {
-    radius   : 40,
-    ratings  : {}
+    radius: 40,
+    ratings: {}
   }
 
-  factorList = Object.keys( colors )
+  factorList = Object.keys(colors)
   size = this.props.radius * 2
   space = this.size * 0.03
-  
+
   angle = 60
-  
+
   rad(deg) {
     return deg * Math.PI / 180
   }
 
 
   getScales(ratings) {
-    let scales = [0.59,0.59,0.59,0.59,0.59,0.59];
-    
-    const groups = rating_groups(ratings);
+    let scales = [0.59, 0.59, 0.59, 0.59, 0.59, 0.59]
+
+    const groups = rating_groups(ratings)
 
     Object.keys(groups).forEach(g => {
-      const rtngs = groups[g];
+      const rtngs = groups[g]
       if (rtngs && rtngs.length > 0) {
-        const av = rtngs.reduce((acc, r) => {return acc+r.value;}, 0) / rtngs.length;
-        const scale = 0.233 * av + 0.533;
+        const av = rtngs.reduce((acc, r) => {
+          return acc + r.value
+        }, 0) / rtngs.length
+        const scale = 0.233 * av + 0.533
         switch (g) {
           case "diversity":
-            scales[0] = scale;
-            break;
+            scales[0] = scale
+            break
           case "renewable":
-            scales[1] = scale;
-            break;
+            scales[1] = scale
+            break
           case "fairness":
-            scales[2] = scale;
-            break;
+            scales[2] = scale
+            break
           case "humanity":
-            scales[3] = scale;
-            break;
+            scales[3] = scale
+            break
           case "transparency":
-            scales[4] = scale;
-            break;
+            scales[4] = scale
+            break
           case "solidarity":
-            scales[5] = scale;
-            break;
+            scales[5] = scale
+            break
           default:
-            break;
+            break
         }
       }
     })
-    return scales;
+    return scales
   }
 
   getTransformation(i, scale, space) {
-    const deg = i * this.angle;
-    let s = scale;
-    if (s == null) { s = 1; }
+    const deg = i * this.angle
+    let s = scale
+    if (s == null) {
+      s = 1
+    }
     return `translate(${space * Math.cos(this.rad(deg))},${space * Math.sin(this.rad(deg))}) \
-      rotate(${deg-90}) \
+      rotate(${deg - 90}) \
       scale(${s})`
   }
 
@@ -78,10 +83,10 @@ class Flower extends Component {
     return (scale === 0.59) ? colors["default"] : colors[factor]
   }
 
-  getTooltip(factorKey){
-    const factor = i18n.t("ratings.contextName."+factorKey)
-    const description = i18n.t("ratings.contextExplanation."+factorKey)
-    return "<h3>"+factor+"</h3><p>"+description+"</p>"
+  getTooltip(factorKey) {
+    const factor = i18n.t("ratings.contextName." + factorKey)
+    const description = i18n.t("ratings.contextExplanation." + factorKey)
+    return "<h3>" + factor + "</h3><p>" + description + "</p>"
 
   }
 
@@ -92,32 +97,32 @@ class Flower extends Component {
     const radius = this.props.radius
 
     const scales = this.getScales(this.props.ratings)
-    
-    
+
+
     const leafs = [0, 1, 2, 3, 4, 5].map((i) => {
-    
+
       const tooltip = this.props.showTooltip ? this.getTooltip(this.factorList[i]) : ''
-      
-      return(
-        <Leaf 
-          key = {i}
-          transform={ this.getTransformation(i, scales[i], this.space) }
-          color={ this.getColor(scales[i], this.factorList[i] ) }
-          height={ h }
-          width={ w }
-          tooltip={ tooltip }
+
+      return (
+        <Leaf
+          key={i}
+          transform={this.getTransformation(i, scales[i], this.space)}
+          color={this.getColor(scales[i], this.factorList[i])}
+          height={h}
+          width={w}
+          tooltip={tooltip}
         />
       )
     })
     return (
       <div>
-        <svg width={(radius +2) * 2} height = {(radius+2) * 2}>
+        <svg width={(radius + 2) * 2} height={(radius + 2) * 2}>
           <g transform={"translate(" + (radius + 2) + "," + (radius + 2) + ")"}>
-            <circle cx={0} cy={0} r={radius+1} fill="#fff" stroke="#ccc" strokeWidth={0.5} />
+            <circle cx={0} cy={0} r={radius + 1} fill="#fff" stroke="#ccc" strokeWidth={0.5}/>
             <g> {leafs} </g>
           </g>
         </svg>
-        <KVTooltip place="bottom" type="dark" effect="solid" />
+        <KVTooltip place="bottom" type="dark" effect="solid"/>
 
       </div>
     )
@@ -125,8 +130,8 @@ class Flower extends Component {
 }
 
 Flower.propTypes = {
-  ratings:  T.array,
-  radius:   T.number,
+  ratings: T.array,
+  radius: T.number,
   showTooltip: T.bool
 }
 
